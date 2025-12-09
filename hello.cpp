29349f6c05ca006f9f978885d1d98fcdcc283de7
mel29 <aanchaltamboli291@gmail.com>
2025-12-09 17:45:24 +0530

    Remove all files

--- hello.cpp
+++
@@ -1,9 +0,0 @@
-#include<iostream>
-using namespace std;
-
-int main(){
-    cout<<"Hello, World!"<<endl;
-    cout<<"This is a basic C++ program."<<endl;
-    cout<<"I am Aanchal Tamboli, learning C++ programming."<<endl;
-    return 0;
-}
